f0c5b3488725f73b811fb08d1485c5ec6540300c
zhangge <zhangge@zhongan.com>
2018-01-16 15:42:25 +0800

    add vux and reset less

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
+@import '~vux/src/styles/reset.less';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  max-width: 10rem;
+  min-width: 320px;
+  margin-left: auto;
+  margin-right: auto;
+  background: #efedec;
 }
 </style>
